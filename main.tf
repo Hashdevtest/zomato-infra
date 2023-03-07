ca49be8461945a6187185571503e22413b252895
hashdev <haashdev313@gmail.com>
2023-03-07 17:47:37 +0000

    2nd security group for SSH access is created

--- main.tf
+++ main.tf
@@ -27,4 +27,44 @@ resource "aws_security_group" "frontend-traffic" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  tags = {
+      
+    "Name" = "${var.project}-${var.environment}-frontend"
+    "project" = var.project
+    "environemnt" = var.environment
+      
+  }
+}
+
+
+resource "aws_security_group" "remote-traffic" {
+    
+  name        = "${var.project}-${var.environment}-remote"
+  description = "Allow SSH traffic"
+    
+ ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  } 
+    
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+      
+    "Name" = "${var.project}-${var.environment}-remote"
+    "project" = var.project
+    "environemnt" = var.environment  
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
