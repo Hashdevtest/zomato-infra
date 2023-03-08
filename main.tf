fa4a1210fdafa29d5a6e22617821f200f99a6074
Haashdev <hashdev@gmail.com>
2023-03-08 18:37:08 +0000

    New port 8080 is opened

--- main.tf
+++ main.tf
@@ -10,7 +10,15 @@ resource "aws_security_group" "frontend-traffic" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   } 
-    
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   ingress {
     from_port        = 443
     to_port          = 443
