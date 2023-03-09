e40c622562107235dc5a44d88b199264f1c9c1bc
Haashdev <hashdev@gmail.com>
2023-03-09 17:41:44 +0000

    db - security group added

--- main.tf
+++ main.tf
@@ -76,6 +76,39 @@ resource "aws_security_group" "remote-traffic" {
   }
 }
 
+
+resource "aws_security_group" "database-traffic" {
+
+  name        = "${var.project}-${var.environment}-database"
+  description = "Allow database access"
+
+ ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    "Name" = "${var.project}-${var.environment}-database"
+    "project" = var.project
+    "environemnt" = var.environment
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
 resource "aws_instance"  "webserver" {
    
   ami = var.instance_ami
